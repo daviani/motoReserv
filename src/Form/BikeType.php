d2f8742f3120bdc5a0672c82dbadb590bf67c83a
Théau <theau26@gmail.com>
2019-07-25 09:04:12 +0200

    Beautify crud

---
+++ src/Form/BikeType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Bike;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BikeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('brand')
+            ->add('model')
+            ->add('matriculation')
+            ->add('color')
+            ->add('power')
+            ->add('km')
+            ->add('productionYear', DateType::class, [
+                'widget' => 'choice',
+                'input_format' => 'Y',
+                'data' => new \DateTime(),
+            ])
+            ->add('status')
+            ->add('tags')
+            ->add('bikeOwner')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Bike::class,
+        ]);
+    }
+}
