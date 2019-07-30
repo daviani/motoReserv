e0ea08df83be8747f8b2dcec91d96b27afcb3caa
Benjamin Georgeault <benjamin@pressop.eu>
2019-07-30 11:35:20 +0200

    ADd an example for Doctrine ORM QueryBuilder.

--- src/Repository/BikeRepository.php
+++ src/Repository/BikeRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\Bike;
+use App\Entity\BikeOwner;
+use App\Entity\Tag;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -20,22 +22,47 @@ class BikeRepository extends ServiceEntityRepository
         parent::__construct($registry, Bike::class);
     }
 
-    // /**
-    //  * @return Bike[] Returns an array of Bike objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @param BikeOwner|null $bikeOwner
+     * @param Tag[] $tags
+     * @return Bike[] Returns an array of Bike objects
+     */
+    public function findOldBikeAvailable(BikeOwner $bikeOwner = null, array $tags = [])
     {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('b.id', 'ASC')
+        $qb = $this->createQueryBuilder('b');
+
+        $qb
+            ->where('b.productionYear < :oldDate')
+            ->andWhere('b.status = 1')
+            ->orderBy('b.productionYear', 'ASC')
             ->setMaxResults(10)
+        ;
+
+        if (null !== $bikeOwner) {
+            $qb
+                ->andWhere('b.bikeOwner = :bikeOwner')
+                ->setParameter('bikeOwner', $bikeOwner)
+            ;
+        }
+
+        if (!empty($tags)) {
+            $qb
+                ->innerJoin('b.tags', 't')
+                ->andWhere('t IN (:tags)')
+                ->setParameter('tags', $tags)
+            ;
+        }
+
+        // Parameters
+        $date = new \DateTime();
+        $date->sub(new \DateInterval('P30Y'));
+        $qb->setParameter('oldDate', $date);
+
+        return $qb
             ->getQuery()
             ->getResult()
         ;
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?Bike
