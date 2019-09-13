f9ef44e5fe0b47615079d25da23c7199fe167cb3
Benjamin Georgeault <benjamin@pressop.eu>
2019-09-13 15:24:17 +0200

    Test Bike's status translation key.

--- tests/Entity/BikeTest.php
+++ tests/Entity/BikeTest.php
@@ -21,7 +21,23 @@ class BikeTest extends TestCase
         $bike->setMatriculation('AZERTY');
 
         $string = 'Foo Bar (AZERTY)';
+        $realString = $bike->__toString();
 
-        $this->assertSame($string, $bike->__toString());
+        $this->assertInternalType('string', $realString);
+        $this->assertSame($string, $realString);
+    }
+
+    public function testStatusTransKey()
+    {
+        $bike = new Bike();
+
+        $this->assertSame('bike.entity.status.default.label', $bike->getStatusTransKey());
+
+        $bike->setStatus(1);
+        $this->assertSame('bike.entity.status.available.label', $bike->getStatusTransKey());
+
+        // ... 2 3 4
+        $bike->setStatus(5);
+        $this->assertSame('bike.entity.status.default.label', $bike->getStatusTransKey());
     }
 }
